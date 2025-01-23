30a27d571de6b31fa4ca35370cfb839f1743ae58
Dominic Smith <dominic.smith@loke.com.au>
2025-01-23 15:32:06 +1100

    Think its almost done

--- src/mem.ts
+++ src/mem.ts
@@ -1,51 +1,35 @@
+import { LRUCache } from "lru-cache";
 import { Cache, Key, Record } from "./cache";
 
-type MapLike<T> = {
-  get(key: Key): T | undefined;
-  set(key: Key, value: T): void;
-  delete(key: Key): void;
-};
+type Options<T> =
+  | {
+      max: number;
+    }
+  | {
+      maxSize: number;
+      sizeCalculation: (value: Record<T>, key: string) => number;
+    };
 
 export class MemCache<T> implements Cache<T> {
-  private timers = new Map<Key, number>();
+  private cache: LRUCache<string, Record<T>>;
 
-  constructor(private cache: MapLike<Record<T>> = new Map()) {}
+  constructor(opts: Options<T>) {
+    this.cache = new LRUCache<string, Record<T>>(opts);
+  }
 
   async get(key: Key): Promise<Record<T> | undefined> {
-    const record = this.cache.get(key);
-
-    if (record === undefined) return undefined;
-
-    if (record.expiresAt < Date.now()) {
-      this.cache.delete(key);
-      return undefined;
-    }
-
-    return record;
+    return this.cache.get(key);
   }
 
   set(key: Key, record: Record<T>): Promise<void> {
     const ttl = record.expiresAt - Date.now();
-    const timer = setTimeout(() => {
-      if (this.cache.get(key) === record) {
-        this.cache.delete(key);
-      }
-    }, ttl);
 
-    this.timers.set(key, timer);
-
-    this.cache.set(key, record);
+    this.cache.set(key, record, { ttl });
 
     return Promise.resolve();
   }
 
   delete(key: Key): Promise<void> {
-    const timer = this.timers.get(key);
-    if (timer !== undefined) {
-      this.timers.delete(key);
-      clearTimeout(timer);
-    }
-
     this.cache.delete(key);
 
     return Promise.resolve();
--- src/redis.ts
+++ src/redis.ts
@@ -1,7 +1,17 @@
 import { Cache, Key, Record } from "./cache";
 
+import type {
+  Redis as IORedisClient,
+  Cluster as IORedisCluster,
+} from "ioredis";
+
+type Client = IORedisClient | IORedisCluster;
+
 export class RedisCache<T> implements Cache<T> {
-  constructor(private client: any, private prefix: string) {}
+  constructor(
+    private client: Client,
+    private prefix: string
+  ) {}
 
   async get(key: Key): Promise<Record<T> | undefined> {
     const rawData = await this.client.get(`${this.prefix}:${key}`);

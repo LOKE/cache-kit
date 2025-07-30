f67c94840dc4573c80ad8c3ed828c01bc3a9407b
Peter Newman <180894268+peter-newman-loke@users.noreply.github.com>
2025-07-30 14:24:36 +1000

    Support prom-client@12

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,6 +1,6 @@
 import { Metric, Registry } from "prom-client";
 
-export const metrics: Metric[] = [];
+export const metrics: Metric<string>[] = [];
 
 export function registerMetrics(registry: Registry): void {
   for (const metric of metrics) {
